08cc9e1594ab4f0cc8521316e27f14c169d3a899
raubritterott <raubritterott@gmail.com>
2026-05-27 22:26:00 +0200

    Remove suspend and resume methods from module

--- MMM-Ecoflow.js
+++ MMM-Ecoflow.js
@@ -47,15 +47,6 @@ Module.register("MMM-Ecoflow",
     setInterval(() => this.getEcoflowData(), this.updateInterval)
   },
 
-  suspend: function () {
-    this.suspended = true;
-  },
-
-  resume: function () {
-    this.suspended = false;
-    this.updateDom(0);
-  }
-
   /**
    * Handle notifications received by the node helper.
    * So we can communicate between the node helper and the module.
