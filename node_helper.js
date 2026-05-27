38ae17c1d1f33ffeb784d7f597c3435170703039
raubritterott <raubritterott@gmail.com>
2026-05-27 21:02:10 +0200

    Refactor Ecoflow data extraction to simplify JSON parsing and improve variable assignments

--- node_helper.js
+++ node_helper.js
@@ -33,28 +33,27 @@ module.exports = NodeHelper.create(
           {
             const json = JSON.parse(rawData);
 
-            const sn = json.body?.sn ?? "";
-            const name = json.body?.name ?? "";
-            const pv1_ampere = json.body?.pv1_ampere === 0 ? "0.00" : json.body?.pv1_ampere ?? "";
-            const pv2_ampere = json.body?.pv2_ampere === 0 ? "0.00" : json.body?.pv2_ampere ?? "";
-            const grid_ampere = json.body?.grid_ampere === 0 ? "0.00" : json.body?.grid_ampere ?? "";
-            const pv1_voltage = json.body?.pv1_voltage === 0 ? "0.00" : json.body?.pv1_voltage ?? "";
-            const pv2_voltage = json.body?.pv2_voltage === 0 ? "0.00" : json.body?.pv2_voltage ?? "";
-            const grid_voltage = json.body?.grid_voltage === 0 ? "0.00" : json.body?.grid_voltage ?? "";
-            const pv1_watts = json.body?.pv1_watts === 0 ? "0.00" : json.body?.pv1_watts ?? "";
-            const pv2_watts = json.body?.pv2_watts === 0 ? "0.00" : json.body?.pv2_watts ?? "";
-            const grid_watts = json.body?.grid_watts === 0 ? "0.00" : json.body?.grid_watts ?? "";
-            const grid_frequency = json.body?.grid_frequency === 0 ? "0.00" : json.body?.grid_frequency ?? "";
-            const temperature = json.body?.temperature === 0 ? "0.00" : json.body?.temperature ?? "";
-            const wifi_rssi = json.body?.wifi_rssi === 0 ? "0.00" : json.body?.wifi_rssi ?? "";
-            const max_power = json.body?.max_power === 0 ? "0.00" : json.body?.max_power ?? "";
-            const state = json.body?.state ?? "Offline";
-            const mqtt_connected = json.body?.mqtt_connected ?? false;
-            const last_update = json.body?.last_update ?? "";
-            const last_full_update = json.body?.last_full_update ?? "";
+            const sn = json.sn ?? "";
+            const name = json.name ?? "";
+            const pv1_ampere = json.pv1_ampere === 0 ? "0.00" : json.body?.pv1_ampere ?? "";
+            const pv2_ampere = json.pv2_ampere === 0 ? "0.00" : json.body?.pv2_ampere ?? "";
+            const grid_ampere = json.grid_ampere === 0 ? "0.00" : json.body?.grid_ampere ?? "";
+            const pv1_voltage = json.pv1_voltage === 0 ? "0.00" : json.body?.pv1_voltage ?? "";
+            const pv2_voltage = json.pv2_voltage === 0 ? "0.00" : json.body?.pv2_voltage ?? "";
+            const grid_voltage = json.grid_voltage === 0 ? "0.00" : json.body?.grid_voltage ?? "";
+            const pv1_watts = json.pv1_watts === 0 ? "0.00" : json.body?.pv1_watts ?? "";
+            const pv2_watts = json.pv2_watts === 0 ? "0.00" : json.body?.pv2_watts ?? "";
+            const grid_watts = json.grid_watts === 0 ? "0.00" : json.body?.grid_watts ?? "";
+            const grid_frequency = json.grid_frequency === 0 ? "0.00" : json.body?.grid_frequency ?? "";
+            const temperature = json.temperature === 0 ? "0.00" : json.body?.temperature ?? "";
+            const wifi_rssi = json.wifi_rssi === 0 ? "0.00" : json.body?.wifi_rssi ?? "";
+            const max_power = json.max_power === 0 ? "0.00" : json.body?.max_power ?? "";
+            const state = json.state ?? "Offline";
+            const mqtt_connected = json.mqtt_connected ?? false;
+            const last_update = json.last_update ?? "";
+            const last_full_update = json.last_full_update ?? "";
 
-            console.log("Ecoflow Response:", json);
-            console.log("EcoflowState:",state)
+            //console.log("Ecoflow Response:", json);
 
             this.sendSocketNotification("ECOFLOW_DATA", { 
                  sn: sn
